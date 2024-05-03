26f26d87dd097fa33aca5f32207cebd081a89c72
thelastbrittain <benjaminpbrittain@gmail.com>
2024-05-02 21:28:46 -0600

    Added overrides for hash and equals as to ChessMove as well

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Objects;
+
 /**
  * Represents moving a chess piece on a chessboard
  * <p>
@@ -45,5 +47,18 @@ public class ChessMove {
     @Override
     public String toString() {
         return "Start Position: " + startPosition + " End Position: " + endPosition + " Promotion Piece: " + promotionPiece;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessMove chessMove = (ChessMove) o;
+        return Objects.equals(startPosition, chessMove.startPosition) && Objects.equals(endPosition, chessMove.endPosition) && promotionPiece == chessMove.promotionPiece;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(startPosition, endPosition, promotionPiece);
     }
 }
