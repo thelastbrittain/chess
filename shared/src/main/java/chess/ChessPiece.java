39d3470a0c04bf0d3b2a66d806fec4b143f099bb
thelastbrittain <benjaminpbrittain@gmail.com>
2024-05-04 15:12:33 -0600

    Idea was correct. After adding overrides to all of the specified classes, the test passed.

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -2,6 +2,7 @@ package chess;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -119,6 +120,19 @@ public class ChessPiece {
     @Override
     public String toString() {
         return type + " " + teamColor;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessPiece that = (ChessPiece) o;
+        return teamColor == that.teamColor && type == that.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(teamColor, type);
     }
 }
 
