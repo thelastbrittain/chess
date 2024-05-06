db182eca6f378799c91807cc3d922c9cd7d050e7
thelastbrittain <benjaminpbrittain@gmail.com>
2024-05-06 17:38:03 -0600

    Refactored PawnMoveCalculator. Passes all tests.

--- shared/src/main/java/chess/PawnMoveCalculator.java
+++ shared/src/main/java/chess/PawnMoveCalculator.java
@@ -22,195 +22,87 @@ public class PawnMoveCalculator {
         this.col = position.getColumn();
     }
 
-    public void calculateMoves(){
-        if (teamColor == ChessGame.TeamColor.WHITE){
-            goUp();
-            goUpLeft();
-            goUpRight();
-        }
-        else {
-            goDown();
-            goDownLeft();
-            goDownRight();
+    public void calculateMoves() {
+        if (teamColor == ChessGame.TeamColor.WHITE) {
+            goDirection(1,0);
+            goDirection(1,1);
+            goDirection(1,-1);
+        } else {
+            goDirection(-1, 0);
+            goDirection(-1, 1);
+            goDirection(-1,-1);
         }
     }
 
     //this function takes in a location. If an enemy or nothing is there, it adds 4 moves to the list, Knight, Rook, Bishop, Queen
-    private void promotePiece(ChessPosition newPosition){
-        for (ChessPiece.PieceType piece : promotionPieces){
+    private void promotePiece(ChessPosition newPosition) {
+        for (ChessPiece.PieceType piece : promotionPieces) {
             ChessMove move = new ChessMove(position, newPosition, piece);  //if this break bc making new everytime, move this two lines up
             ogList.add(move);
         }
 
     }
 
-    private void goDirection(int rowChange, int colChange){
+    private void goDirection(int rowChange, int colChange) {
+        //creating variables for promotion/doubleMove
+        boolean doubleMove = false;
+        boolean promotion = false;
         //create new row and col
         int newRow = row + rowChange;
         int newCol = col + colChange;
         //make sure new rol/col are in bounds
-        if (newRow > 8 || newCol > 8 || newRow < 0 || newCol < 0){return;}
+        if (newRow > 8 || newCol > 8 || newRow < 0 || newCol < 0) {
+            return;
+        }
 
-        //if type is white,
-            //if start row is 2 {check double up = true}
-            //if start is 7 {promotion = true}
-        //if type is black,
-            //if row is 7 {doubleUp = true}
-            //if row is 2 {promotion = true}
-        if (teamColor == ChessGame.TeamColor.WHITE){
-            if (row == 2){
+        //setting the promotion and double move bools
+        if (teamColor == ChessGame.TeamColor.WHITE) {
+            if (row == 2 && colChange == 0) {
+                doubleMove = true;
+            }
+            if (row == 7) {
+                promotion = true;
+            }
+        }
+        if (teamColor == ChessGame.TeamColor.BLACK) {
+            if (row == 7 && colChange == 0) {
+                doubleMove = true;
+            }
+            if (row == 2) {
+                promotion = true;
+            }
         }
 
         //make new position
         //get the piece at the new position
+        ChessPosition newPosition = new ChessPosition(newRow, newCol);
+        ChessPiece newPiece = board.getPiece(newPosition);
 
-        //if col change == 0
-            //if place is empty
-                //if promotion
-                //add move
-                //if double move
-                    //add another move
-
-        //else
-            //if space not empty and space is enemy team
-                //if promotion
-                    //add 4 moves
-                //add move
-
-
-
-    }
-
-
-    private void goUp() {
-        if (this.row == 8){return;}
-        ChessPosition upPosition = new ChessPosition(row + 1, col);  //creating object to view up position
-
-        //double up case
-        if (this.row == 2) {
-            if (board.getPiece(upPosition) == null) {          //if nothing there, create new move and add to list
-                ChessMove upPiece = new ChessMove(position, upPosition, null);
-                ogList.add(upPiece);
-
-                ChessPosition doubleUpPosition = new ChessPosition(row + 2, col);  //now looking at space two up
-                if (board.getPiece(doubleUpPosition) == null) {//if nothing there add move
-                    ChessMove doubleUpPiece = new ChessMove(position, doubleUpPosition, null);
-                    ogList.add(doubleUpPiece);
+        if (colChange == 0) {
+            if (newPiece == null) {
+                if (promotion) {
+                    promotePiece(newPosition);
+                } else {
+                    ChessMove newMove = new ChessMove(position, newPosition, null);
+                    ogList.add(newMove);
+                    if (doubleMove) {
+                        ChessPosition doublePosition = new ChessPosition(newRow + rowChange, newCol);
+                        ChessPiece testPiece = board.getPiece(doublePosition);
+                        if (testPiece == null) {
+                            ChessMove newDoubleMove = new ChessMove(position, doublePosition, null);
+                            ogList.add(newDoubleMove);
+                        }
+                    }
                 }
             }
-        }
-        //promotion case
-        else if (this.row == 7) {
-            if (board.getPiece(upPosition) == null) {
-                promotePiece(upPosition);
-            }
-        }
-
-        else {
-            if (this.row != 8){
-                if (board.getPiece(upPosition) == null) {                        //of nothing there, create new move and add to list
-                    ChessMove upPiece = new ChessMove(position, upPosition, null);
-                    ogList.add(upPiece);
+        } else {
+            if (newPiece != null && newPiece.getTeamColor() != teamColor) {
+                if (promotion) {
+                    promotePiece(newPosition);}
+                else {
+                    ChessMove newMove = new ChessMove(position, newPosition, null);
+                    ogList.add(newMove);
                 }
-            }
-        }
-    }
-
-
-    private void goUpRight() {
-        if (this.col == 8) {
-            return;}
-        ChessPosition upRightPosition = new ChessPosition(row + 1, col + 1);  //creating object to view up position
-
-        if (board.getPiece(upRightPosition) != null && board.getPiece(upRightPosition).getTeamColor() == ChessGame.TeamColor.BLACK) {
-            if (this.row == 7) {
-                promotePiece(upRightPosition);
-            } else {
-                ChessMove upRightPiece = new ChessMove(position, upRightPosition, null);
-                ogList.add(upRightPiece);
-            }
-
-        }
-    }
-
-    private void goUpLeft() {
-        if (this.col == 1) {
-            return;
-        }
-        ChessPosition upLeftPosition = new ChessPosition(row + 1, col - 1);  //creating object to view up position
-
-        if (board.getPiece(upLeftPosition) != null && board.getPiece(upLeftPosition).getTeamColor() == ChessGame.TeamColor.BLACK) {                        //of nothing there, create new move and add to list
-            if (this.row == 7) {
-                promotePiece(upLeftPosition);
-            }else {
-                ChessMove upLeftPiece = new ChessMove(position, upLeftPosition, null);
-                ogList.add(upLeftPiece);
-            }
-
-        }
-    }
-
-    private void goDown() {
-        //break if on the edge of the board
-        if (this.row == 1){return;}
-        ChessPosition downPosition = new ChessPosition(row - 1, col);  //creating object to view up position
-
-        //double up case
-        if (this.row == 7) {
-            if (board.getPiece(downPosition) == null) {          //if nothing there, create new move and add to list
-                ChessMove upPiece = new ChessMove(position, downPosition, null);
-                ogList.add(upPiece);
-
-                ChessPosition doubleDownPosition = new ChessPosition(row - 2, col);  //now looking at space two up
-                if (board.getPiece(doubleDownPosition) == null) {//if nothing there add move
-                    ChessMove doubleDownPiece = new ChessMove(position, doubleDownPosition, null);
-                    ogList.add(doubleDownPiece);
-                }
-            }
-        }
-        //promotion case
-        else if (this.row == 2) {
-            if (board.getPiece(downPosition) == null) {
-                promotePiece(downPosition);
-            }
-        }
-        //every other case
-        else {
-            if (board.getPiece(downPosition) == null) {                        //of nothing there, create new move and add to list
-                ChessMove downPiece = new ChessMove(position, downPosition, null);
-                ogList.add(downPiece);
-            }
-        }
-    }
-
-    private void goDownRight() {
-        //break if on the edge
-        if (this.col == 8) {
-            return;}
-        ChessPosition downRightPosition = new ChessPosition(row - 1, col + 1);  //creating object to view up position
-
-        if (board.getPiece(downRightPosition) != null && board.getPiece(downRightPosition).getTeamColor() == ChessGame.TeamColor.WHITE) {
-            if (this.row == 2) {
-                promotePiece(downRightPosition);
-            } else {
-                ChessMove downRightPiece = new ChessMove(position, downRightPosition, null);
-                ogList.add(downRightPiece);
-            }
-
-        }
-    }
-
-    private void goDownLeft() {
-        if (this.col == 1) {
-            return;
-        }
-        ChessPosition downLeftPosition = new ChessPosition(row - 1, col - 1);  //creating object to view up position
-        if (board.getPiece(downLeftPosition) != null && board.getPiece(downLeftPosition).getTeamColor() == ChessGame.TeamColor.WHITE) {                        //of nothing there, create new move and add to list
-            if (this.row == 2) {
-                promotePiece(downLeftPosition);
-            }else {
-                ChessMove downLeftPiece = new ChessMove(position, downLeftPosition, null);
-                ogList.add(downLeftPiece);
             }
         }
     }
