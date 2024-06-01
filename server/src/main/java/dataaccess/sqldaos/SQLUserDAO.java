f50b694dc5a6f9670b12af4b9c312defaab025cd
Ben Brittain <38736115+thelastbrittain@users.noreply.github.com>
2024-06-01 16:43:30 -0600

    Encypted and decrypted passwords. Passed all tests. Now just need to write service tests.

--- server/src/main/java/dataaccess/sqldaos/SQLUserDAO.java
+++ server/src/main/java/dataaccess/sqldaos/SQLUserDAO.java
@@ -4,6 +4,7 @@ import dataaccess.DataAccessException;
 import dataaccess.DatabaseManager;
 import dataaccess.interfaces.UserDAO;
 import model.UserData;
+import org.mindrot.jbcrypt.BCrypt;
 
 import java.sql.SQLException;
 
@@ -18,8 +19,9 @@ public class SQLUserDAO implements UserDAO {
     @Override
     public UserData createUser(UserData newUser) {
         var statement = "INSERT INTO user (username, password, email) VALUES (?, ?, ?)";
+        String hashedPassword = BCrypt.hashpw(newUser.password(), BCrypt.gensalt());
         try {
-            var id = executeUpdate(statement, newUser.username(), newUser.password(), newUser.email());
+            var id = executeUpdate(statement, newUser.username(), hashedPassword, newUser.email());
         } catch (DataAccessException e) {
             System.out.println("Problem in inserting a user: "  + e.getMessage());
             return new UserData(null,null,null);
@@ -63,26 +65,26 @@ public class SQLUserDAO implements UserDAO {
 
     @Override
     public boolean isVerifiedUser(String username, String password) {
-        String query = "SELECT COUNT(*) FROM user WHERE username = ? AND password = ?";
+        String query = "SELECT password FROM user WHERE username = ?";
 
         try (var conn = DatabaseManager.getConnection();
              var ps = conn.prepareStatement(query)) {
+
             ps.setString(1, username);
-            ps.setString(2, password);
 
             try (var rs = ps.executeQuery()) {
                 if (rs.next()) {
-                    int count = rs.getInt(1);
-                    return count > 0;
+                    String hashedPassword =  rs.getString("password");
+                    return BCrypt.checkpw(password, hashedPassword);
+                } else {
+                    // No password found for the given username
+                    return false;
                 }
             }
-        } catch (SQLException e) {
-            System.out.println("Error checking user credentials: " + e.getMessage());
+        } catch (SQLException | DataAccessException e) {
+            System.out.println("SQL Error retreving password: " + e.getMessage());
             return false;
-        } catch (DataAccessException e) {
-            System.out.println(e.getMessage());
         }
-        return false;
     }
 }
 
