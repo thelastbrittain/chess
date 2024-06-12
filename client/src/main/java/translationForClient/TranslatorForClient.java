7a59a351243bf58bf3c887f50ec7d39312f1cd20
Ben Brittain <38736115+thelastbrittain@users.noreply.github.com>
2024-06-12 09:26:40 -0600

    Added typeAdapter to the TranslatorForClient

--- client/src/main/java/translationForClient/TranslatorForClient.java
+++ client/src/main/java/translationForClient/TranslatorForClient.java
@@ -1,9 +1,14 @@
 package translationForClient;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import typeadapter.CommandTypeAdapter;
+import typeadapter.ServerMessageTypeAdapter;
+import websocket.commands.UserGameCommand;
+import websocket.messages.ServerMessage;
 
 public class TranslatorForClient {
-    private static Gson gson = new Gson();
+    private static Gson gson = prepareGson();
 
     public static Object fromObjectToJson(Object result){
         return gson.toJson(result);
@@ -11,6 +16,12 @@ public class TranslatorForClient {
 
     public static <T> T fromJsontoObjectNotRequest(String string, Class<T> classOfT){
         return gson.fromJson(string, classOfT);
+    }
+
+    private static Gson prepareGson(){
+        GsonBuilder builder = new GsonBuilder();
+        builder.registerTypeAdapter(ServerMessage.class, new ServerMessageTypeAdapter());
+        return builder.create();
     }
 
 }
